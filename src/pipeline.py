38ce3732ed10f841aa271ad917fef7e767880297
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2024-03-01 16:10:47 +0900

    [wip] add interface

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 
+import streamlit as st
 from checkworthy import identify_checkworthiness
 from decompose import decompose_document_into_claims
 from retrieval import chunk_document, create_elasticsearch_client, create_relevance_scorer, search_documents
@@ -35,59 +36,80 @@ def main(args: argparse.Namespace) -> None:
     Args:
         args (argparse.Namespace): The command-line arguments.
     """
-    logger.info("Running the fact-checking pipeline.")
-    logger.info(f"Document: {args.document}")
-    logger.info(f"Context: {args.context}")
+    st.title("LLM-jp Fact-Check")
 
-    logger.info("Decompose the document into claims.")
-    claims = decompose_document_into_claims(
-        document=args.document,
-        context=args.context,
-        model=args.engine,
-    )
-    logger.info(f"Extracted {len(claims)} claims.")
-    logger.debug(f"Claims: {claims}")
+    with st.form("form", clear_on_submit=False):
+        st.write("Please enter the document (and the context if applicable) you want to fact-check.")
+        context = st.text_area("Context")
+        document = st.text_area("Document")
+        submitted = st.form_submit_button("Submit")
 
-    logger.info("Identify check-worthy claims.")
-    checkworthy_labels = identify_checkworthiness(claims, args.engine)
-    checkworthy_claims = [claim for claim, label in zip(claims, checkworthy_labels) if label]
-    logger.info(f"Identified {len(checkworthy_claims)} check-worthy claims.")
-    logger.debug(f"Check-worthy claims: {checkworthy_claims}")
+    if submitted:
+        with st.spinner("Decomposing the document into claims..."):
+            claims = decompose_document_into_claims(
+                document=document,
+                context=context,
+                model=args.engine,
+            )
 
-    logger.info("Retrieve evidence documents.")
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
-    es = create_elasticsearch_client(args.es_host)
-    scorer = create_relevance_scorer(args.embedding)
-    evidences = []
-    for claim in checkworthy_claims:
-        claim_token_ids = tokenizer(claim, add_special_tokens=False)["input_ids"]
-        hits = search_documents(
-            es,
-            args.es_index,
-            body={"query": {"match": {"token_ids": " ".join(map(str, claim_token_ids))}}},
-            size=3,
-        )
-        evidences_of_claim = []
-        for hit in hits:
-            document = tokenizer.decode(list(map(int, hit["_source"]["token_ids"].split()))).strip()
-            dataset = hit["_source"]["dataset_name"]
-            training_step = hit["_source"]["iteration"]
-            for passage in chunk_document(document):
-                score = scorer(claim, passage)
-                evidences_of_claim.append((passage, dataset, training_step, score))
+        with st.spinner("Identifying check-worthy claims..."):
+            checkworthy_labels = identify_checkworthiness(claims, args.engine)
+            checkworthy_claims = [claim for claim, label in zip(claims, checkworthy_labels) if label]
 
-        evidences_of_claim.sort(key=lambda x: x[1], reverse=True)
-        evidences.append(evidences_of_claim[: args.num_evidences])
+        @st.cache_resource
+        def _get_tokenizer(tokenizer_name_or_path: str):
+            return AutoTokenizer.from_pretrained(tokenizer_name_or_path)
 
-    logger.info("Verify the check-worthy claims.")
-    for claim, evidences_of_claim in zip(checkworthy_claims, evidences):
-        result = verify_claim(claim, evidences_of_claim, args.engine)
-        logger.info(f"Claim: {claim.strip()}")
-        logger.info(f"Result: {result['label']}")
-        logger.info(f"Rationale: {result['rationale']}")
-        logger.info("Evidence:")
-        for i, (passage, dataset, training_step, _) in enumerate(evidences_of_claim, 1):
-            logger.info(f"{i}. Dataset: {dataset} Training Step: {training_step}\n{passage}")
+        @st.cache_resource
+        def _create_elasticsearch_client(host: str):
+            return create_elasticsearch_client(host)
+
+        @st.cache_resource
+        def _create_relevance_scorer(embedding: str):
+            return create_relevance_scorer(embedding)
+
+        with st.spinner("Retrieving evidence documents..."):
+            tokenizer = _get_tokenizer(args.tokenizer_name)
+            es = _create_elasticsearch_client(args.es_host)
+            scorer = _create_relevance_scorer(args.embedding)
+
+            evidences = []
+            for claim in checkworthy_claims:
+                claim_token_ids = tokenizer(claim, add_special_tokens=False)["input_ids"]
+                hits = search_documents(
+                    es,
+                    args.es_index,
+                    body={"query": {"match": {"token_ids": " ".join(map(str, claim_token_ids))}}},
+                    size=3,
+                )
+                evidences_of_claim = []
+                for hit in hits:
+                    document = tokenizer.decode(list(map(int, hit["_source"]["token_ids"].split()))).strip()
+                    dataset = hit["_source"]["dataset_name"]
+                    training_step = hit["_source"]["iteration"]
+                    for passage in chunk_document(document):
+                        score = scorer(claim, passage)
+                        evidences_of_claim.append((passage, dataset, training_step, score))
+
+                evidences_of_claim.sort(key=lambda x: x[1], reverse=True)
+                evidences.append(evidences_of_claim[: args.num_evidences])
+
+        with st.spinner("Verifying the check-worthy claims..."):
+            results = []
+            for claim, evidences_of_claim in zip(checkworthy_claims, evidences):
+                results.append(verify_claim(claim, evidences_of_claim, args.engine))
+
+        st.subheader("Results")
+        for claim, result_of_claim, evidences_of_claim in zip(checkworthy_claims, results, evidences):
+            st.markdown("---")
+            st.markdown(f"**Claim**: {claim.strip()}")
+            st.markdown(f"**Result**: {result_of_claim['label']}")
+            st.markdown(f"**Rationale**: {result_of_claim['rationale']}")
+            with st.expander("Evidence"):
+                for i, (passage, dataset, training_step, _) in enumerate(evidences_of_claim, 1):
+                    st.markdown(f"Evidence {i}. Dataset: {dataset}. Training Step: {training_step}.")
+                    st.markdown(passage)
+                    st.markdown("---")
 
 
 if __name__ == "__main__":
